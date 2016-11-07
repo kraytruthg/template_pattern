df4be2ee7bd24c03467c8f47c4d1e6b198357939
Kai Sun <tonysun329@gmail.com>
2016-11-07 19:58:29 +0800

    add base class

--- lib/template_pattern.rb
+++ lib/template_pattern.rb
@@ -1,5 +1,36 @@
 require "template_pattern/version"
 
 module TemplatePattern
-  # Your code goes here...
+  class Order
+    attr_reader :opts
+
+    def initialize(**opts)
+      @opts = opts
+    end
+
+    def process_order
+      pick_goods
+      process_payment
+      package
+      deliver
+    end
+
+    private
+
+    def collect_goods
+      fail NotImplementedError
+    end
+
+    def process_payment
+      fail NotImplementedError
+    end
+
+    def package
+      fail NotImplementedError
+    end
+
+    def deliver
+      fail NotImplementedError
+    end
+  end
 end
