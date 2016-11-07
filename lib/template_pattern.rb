0feed5c79fffd29559de92c7944bee6ca1fe6422
chrislintw <q26766@gmail.com>
2016-11-07 20:30:41 +0800

    refactor

--- lib/template_pattern.rb
+++ lib/template_pattern.rb
@@ -1,5 +1,40 @@
 require "template_pattern/version"
 
 module TemplatePattern
-  # Your code goes here...
+  class Order
+
+    def initialize(is_gift: false)
+      @is_gift = is_gift
+    end
+
+    def process_order
+      choose_goods
+      process_payment
+      package
+      deliver
+    end
+
+    private
+
+    def choose_goods
+      raise NotImplementedError
+    end
+
+    def process_payment
+      raise NotImplementedError
+    end
+
+    def package
+      puts "Wraps goods" if is_gift?
+    end
+
+    def deliver
+      raise NotImplementedError
+    end
+
+    def is_gift?
+      raise NotImplementedError
+    end
+  end
+
 end
--- lib/template_pattern/store_order.rb
+++ lib/template_pattern/store_order.rb
@@ -1,14 +1,4 @@
-class StoreOrder
-  def initialize(is_gift: false)
-    @is_gift = is_gift
-  end
-
-  def process_order
-    choose_goods
-    process_payment
-    package if is_gift?
-    deliver
-  end
+class StoreOrder < TemplatePattern::Order
 
   private
 
@@ -20,10 +10,6 @@ class StoreOrder
     puts "Pays at counter through POS"
   end
 
-  def package
-    puts "Wraps goods"
-  end
-
   def deliver
     puts "Delivers in counter"
   end
@@ -31,4 +17,5 @@ class StoreOrder
   def is_gift?
     @is_gift
   end
+
 end
