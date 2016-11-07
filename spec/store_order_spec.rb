59b1e1066926da4e3e1f1b9683d1bd09b4566977
Kai Sun <tonysun329@gmail.com>
2016-11-07 20:39:10 +0800

    alter spec

--- spec/store_order_spec.rb
+++ spec/store_order_spec.rb
@@ -1,17 +1,17 @@
 require "spec_helper"
-require "template_pattern/store_order"
+require "template_pattern"
 
-describe StoreOrder do
+describe "StoreOrder" do
   describe "#process_order" do
     it "outputs process flow" do
-      expect { StoreOrder.new.process_order }.to output(
+      expect { WhatTheHellIsThis::OrderProcessor.process(:store) }.to output(
         "Customer chooses item from shelf\nPays at counter through POS\nDelivers in counter\n"
       ).to_stdout
     end
 
     context "when it is gift order" do
       it "outputs process flow" do
-        expect { StoreOrder.new(is_gift: true).process_order }.to output(
+        expect { WhatTheHellIsThis::OrderProcessor.process(:store, is_gift: true) }.to output(
           "Customer chooses item from shelf\nPays at counter through POS\nWraps goods\nDelivers in counter\n"
         ).to_stdout
       end
